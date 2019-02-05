9d652b71527d5153945f7d3a905dade4d1953bb4
ckiesling20 <ckiesling20@dtechhs.org>
2019-02-05 10:12:10 -0800

    a4 v1

--- classes/IntArrayWorker.java
+++ classes/IntArrayWorker.java
@@ -1,3 +1,5 @@
+// package com.gradescope.pixLab;
+
 public class IntArrayWorker
 {
   /** two dimensional matrix */
@@ -27,11 +29,51 @@ public class IntArrayWorker
     }
     return total;
   }
+
+  public int getCount(int target) {
+    int count = 0;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+          if (matrix[row][col] == target) {
+            count = count + 1;
+
+          }
+      }
+    }
+    return count;
+  }
+
+  public int getLargest() {
+    int largest = matrix[0][0];
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+        if (matrix[row][col] > largest) {
+          largest = matrix[row][col];
+
+        }
+      }
+    }
+    return largest;
+  }
+
+  public int getColTotal(int col) {
+    int total = 0;
+    for (int row = 0; row < matrix.length; row++) {
+      total = total + matrix[row][col];
+    }
+    return total;
+  }
+
   
   /**
    * Method to return the total using a nested for-each loop
    * @return the total of the values in the array
    */
+
   public int getTotalNested()
   {
     int total = 0;
